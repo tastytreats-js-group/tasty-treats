965dbba7be9a2f02ad7737c5c941031c01576ef1
EmineBusraAcici <eminebusraacici@hotmail.com>
2026-02-21 19:14:15 +0300

    Add themes.css file and Edit files

---
+++ src/js/header.js
@@ -0,0 +1,70 @@
+const mobilMenu = document.querySelector('.mobil-menu');
+const exitBtn = document.querySelector('.exit-icon-wrapper');
+const menutBtn = document.querySelector('.menu-icon-wrapper');
+
+menutBtn.addEventListener('click', () => {
+  mobilMenu.classList.toggle('menu-active');
+});
+
+exitBtn.addEventListener('click', () => {
+  mobilMenu.classList.remove('menu-active');
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 768) {
+    mobilMenu.classList.remove('menu-active');
+  }
+});
+
+const root = document.documentElement;
+const body = document.querySelector('body');
+
+const mobileToggle = document.querySelector('.mobile-toggle-switch');
+const desktopToggle = document.querySelector('.desktop-toggle-switch'); 
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+    if (savedTheme === 'dark') {
+      root.setAttribute('data-theme', 'dark');
+      body.setAttribute('data-theme', 'dark');
+    } else {
+      root.removeAttribute('data-theme');
+      body.removeAttribute('data-theme');
+    }
+  } else {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      root.setAttribute('data-theme', 'dark');
+      body.setAttribute('data-theme', 'dark');
+    }
+  }
+ 
+function toggleTheme() {
+  const isDark = root.getAttribute('data-theme') === 'dark';
+
+  if (isDark) {
+    root.removeAttribute('data-theme');
+    body.removeAttribute('data-theme');
+    localStorage.setItem('theme', 'light');
+  } else {
+    root.setAttribute('data-theme', 'dark');
+    body.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+}
+
+mobileToggle?.addEventListener('click', toggleTheme);
+desktopToggle?.addEventListener('click', toggleTheme);
+
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', e => {
+    if (!localStorage.getItem('theme')) {
+      if (e.matches) {
+        root.setAttribute('data-theme', 'dark');
+        body.setAttribute('data-theme', 'dark');
+      } else {
+        root.removeAttribute('data-theme');
+        body.removeAttribute('data-theme');
+      }
+    }
+  });
