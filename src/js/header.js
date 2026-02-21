c2619e97a3389e6543575c4779397b1c27c77aa4
EmineBusraAcici <eminebusraacici@hotmail.com>
2026-02-21 19:32:51 +0300

    Minor changes

--- src/js/header.js
+++ src/js/header.js
@@ -16,7 +16,7 @@ window.addEventListener('resize', () => {
   }
 });
 
-const root = document.documentElement;
+
 const body = document.querySelector('body');
 
 const mobileToggle = document.querySelector('.mobile-toggle-switch');
@@ -26,28 +26,24 @@ const savedTheme = localStorage.getItem('theme');
 
 if (savedTheme) {
     if (savedTheme === 'dark') {
-      root.setAttribute('data-theme', 'dark');
-      body.setAttribute('data-theme', 'dark');
+ body.setAttribute('data-theme', 'dark');
     } else {
-      root.removeAttribute('data-theme');
       body.removeAttribute('data-theme');
     }
   } else {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      root.setAttribute('data-theme', 'dark');
       body.setAttribute('data-theme', 'dark');
     }
   }
  
 function toggleTheme() {
-  const isDark = root.getAttribute('data-theme') === 'dark';
+  const isDark = body.getAttribute('data-theme') === 'dark';
 
   if (isDark) {
-    root.removeAttribute('data-theme');
+
     body.removeAttribute('data-theme');
     localStorage.setItem('theme', 'light');
   } else {
-    root.setAttribute('data-theme', 'dark');
     body.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
   }
@@ -60,10 +56,8 @@ desktopToggle?.addEventListener('click', toggleTheme);
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', e => {
     if (!localStorage.getItem('theme')) {
       if (e.matches) {
-        root.setAttribute('data-theme', 'dark');
         body.setAttribute('data-theme', 'dark');
       } else {
-        root.removeAttribute('data-theme');
         body.removeAttribute('data-theme');
       }
     }
