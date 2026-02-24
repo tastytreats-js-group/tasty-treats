b968ee7e4e2764f5af22b8d834f38b0e06b20462
elif-koseoglu <nuryngn@gmail.com>
2026-02-24 14:45:55 +0000

    Deploying to gh-pages from @ tastytreats-js-group/tasty-treats@91486bf50dce7e2018795237fe0dc3081674d403 🚀

--- favorites.js
+++ favorites.js
@@ -1,4 +1,4 @@
-import"./assets/header-CnC7bpm-.js";const d="favorite-recipes";function u(){try{const t=localStorage.getItem(d);return t?JSON.parse(t):[]}catch(t){return console.error("Storage parse error:",t),[]}}const a=document.querySelector(".js-favorites-list"),s=document.querySelector(".js-favorites-categories"),m=document.querySelector(".js-favorites-empty");let r=[],n="All";document.addEventListener("DOMContentLoaded",g);function g(){if(r=u(),!r.length){f();return}o(),i(r)}function f(){m.classList.remove("is-hidden"),a.classList.add("is-hidden"),s.classList.add("is-hidden")}function i(t){a.innerHTML=t.map(p).join("")}function p(t){return`
+import{g as d}from"./assets/local_favorites-mdPmc23F.js";const a=document.querySelector(".js-favorites-list"),i=document.querySelector(".js-favorites-categories"),u=document.querySelector(".js-favorites-empty");let n=[],s="All";document.addEventListener("DOMContentLoaded",m);function m(){if(n=d(),!n.length){g();return}c(),r(n)}function g(){u.classList.remove("is-hidden"),a.classList.add("is-hidden"),i.classList.add("is-hidden")}function r(t){a.innerHTML=t.map(f).join("")}function f(t){return`
     <li class="recipe-card">
       <img 
         src="${t.thumb}" 
@@ -15,12 +15,12 @@ import"./assets/header-CnC7bpm-.js";const d="favorite-recipes";function u(){try{
         </div>
       </div>
     </li>
-  `}function o(){const t=["All",...new Set(r.map(e=>e.category))];s.innerHTML=t.map(e=>`
+  `}function c(){const t=["All",...new Set(n.map(e=>e.category))];i.innerHTML=t.map(e=>`
       <button 
-        class="category-btn ${e===n?"active":""}"
+        class="category-btn ${e===s?"active":""}"
         data-category="${e}"
       >
         ${e}
       </button>
-    `).join(""),v()}function v(){s.querySelectorAll(".category-btn").forEach(e=>{e.addEventListener("click",()=>{n=e.dataset.category;const c=n==="All"?r:r.filter(l=>l.category===n);o(),i(c)})})}
+    `).join(""),p()}function p(){i.querySelectorAll(".category-btn").forEach(e=>{e.addEventListener("click",()=>{s=e.dataset.category;const o=s==="All"?n:n.filter(l=>l.category===s);c(),r(o)})})}
 //# sourceMappingURL=favorites.js.map
